a4eb629ff990a800445111d3241c5f472be932bd
Silverthy <75179031+FleurDll@users.noreply.github.com>
2021-02-16 09:56:23 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 const apiKey = "0d19090abb5a0f99a36820be42fa1bcc";
-// Today's date
-const optionsDate = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-};
-const dateComplete = (new Date().toLocaleDateString("fr-FR", optionsDate)).toUpperCase();
-$(".date").text(dateComplete);
 
-// Weekday
-const optionsDay = {
-    weekday: "long"
-};
-const currentDay = new Date().toLocaleDateString("fr-FR", optionsDay);
-const daySliced = (currentDay.slice(0, 3)).toUpperCase();
+// Function to search time / date / next 4 days
+let d = new Date();
+let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
 
-const weekday = ["DIM", "LUN", "MAR", "MER", "JEU", "VEN", "SAM"];
-
-const today = (day) => day == String(daySliced);
-const indexCurrentDay = weekday.findIndex(today);
-
-const indexFutur = [];
-
-// Get next days index
-for (i = 1; i <= 4; i++) {
-    const newIndex = (indexCurrentDay + i) % 7;
-    indexFutur.push(newIndex);
+function calcTime(offset) { // get time according to utc 
+    let nd = new Date(utc + (3600000 * offset));
+    const optionsLocalTime = {
+        hour: "numeric"
+    };
+    const time = nd.toLocaleTimeString("fr-FR", optionsLocalTime);
+    const timeSliced = time.slice(0, 2);
+    return timeSliced;
 }
 
-// Insert next days in html page
-for (i = 1; i <= 4; i++) {
-    $(".d" + i).text(weekday[indexFutur[i - 1]]);
-};
+function calcDate(offset) { // get date according to utc
+    let nd = new Date(utc + (3600000 * offset));
+    const optionsLocalDate = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    const dateComplete = (nd.toLocaleString("fr-FR", optionsLocalDate)).toUpperCase();
+    $(".date").text(dateComplete);
+}
 
+// Weekday
+function getNext4Days(offset) {
+    let nd = new Date(utc + (3600000 * offset));
+    const optionsLocalDate = {
+        weekday: "long"
+    };
+    const currentDate = ((nd.toLocaleString("fr-FR", optionsLocalDate)).slice(0, 3)).toUpperCase();
+
+    const weekday = ["DIM", "LUN", "MAR", "MER", "JEU", "VEN", "SAM"];
+
+    const today = (day) => day == String(currentDate);
+    const indexCurrentDay = weekday.findIndex(today);
+    const indexFutur = [];
+
+    // Get next days index
+    for (i = 1; i <= 4; i++) {
+        const newIndex = (indexCurrentDay + i) % 7;
+        indexFutur.push(newIndex);
+        $(".d" + i).text(weekday[indexFutur[i - 1]]);
+    }
+}
 //////////////////////////////////////////////////////////////// Location current weather
 const successLocationCurrent = function (data) {
+    const currentUTC = (data.timezone) / 3600;
+
+    calcDate(currentUTC); // get location date 
+    getNext4Days(currentUTC);
+
     const currentTemp = Math.floor(data.main.temp);
     const currentIcon = data.weather[0].icon;
     const currentSrcIcon = "images/iconSmile/" + currentIcon + ".svg";
 
     $(".icon-today-meteo").attr("src", currentSrcIcon);
     $(".temp").text(currentTemp + "°");
-
 
     getDynamicImage(currentIcon);
 }
@@ -52,16 +69,6 @@ const successLocationForecast = function (data) {
     const locationCountry = data.city.country;
     const locationFlag = "https://purecatamphetamine.github.io/country-flag-icons/3x2/" + locationCountry + ".svg";
 
-    LocationTempForecast = [];
-    LocationIconForecast = [];
-    iconCode = [];  // for dark/white mode
-
-    for (i = 8; i <= 32; i += 8) {
-        LocationTempForecast.push(Math.floor(data.list[i - 1].main.temp));
-        iconCode.push(data.list[i - 1].weather[0].icon);
-        LocationIconForecast.push("images/iconFutur/" + data.list[i - 1].weather[0].icon + ".svg");
-    }
-
     $(".loading").addClass("hidden");
     $("img").removeClass("hidden");
     $(".forecast-meteo").removeClass("hidden");
@@ -69,11 +76,20 @@ const successLocationForecast = function (data) {
     $(".country").text(locationCountry);
     $(".flag-img").attr("src", locationFlag);
 
+    // Get the right icon / temp according to UTC-0 
+    const locationUTC = (data.city.timezone) / 3600;
+    const locationData = data;
+
+    tempsDays = []; // store the next 4 days temp
+    iconDays = []; // store the next 4 days icons
+    iconCode = [];  // for dark/white mode
+
+    handleForecastInfo(locationUTC, locationData);
+
     for (i = 1; i <= 4; i++) {
-        $(".d" + i + "-temp").text(LocationTempForecast[i - 1] + "°");
-        $(".d" + i + "-img").attr("src", LocationIconForecast[i - 1]);
+        $(".d" + i + "-temp").text(tempsDays[i - 1] + "°");
+        $(".d" + i + "-img").attr("src", iconDays[i - 1]);
     }
-    /*  return iconCode; */
 }
 
 /////////////////////////////////////////// API CALL LOCATION
@@ -110,6 +126,7 @@ function error(err) {
     console.warn("ERREUR" + err.code + " " + err.message);
     $(".modal-body").text("Seule la recherche par ville est disponible. (" + err.message + ").");
     $('#myModal').modal('show');
+    $(".loading").addClass("hidden");
 }
 
 if (navigator.geolocation) {
@@ -121,6 +138,11 @@ if (navigator.geolocation) {
 
 //////////////////////////////////////////////////////////////// Submit current weather
 const successSubmitCurrent = function (data) {
+    const localUTC = (data.timezone) / 3600;
+
+    calcDate(localUTC); //get searched city date 
+    getNext4Days(localUTC);
+
     const currentTemp = Math.floor(data.main.temp);
     const currentIcon = data.weather[0].icon;
     const currentSrcIcon = "images/iconSmile/" + currentIcon + ".svg";
@@ -130,27 +152,12 @@ const successSubmitCurrent = function (data) {
 
     getDynamicImage(currentIcon);
 }
+
 /////////////////////////////////////////////////////////////// Submit forcast weather
 const successSubmitForecast = function (data) {
     const searchedCity = (data.city.name).toUpperCase();
     const searchedCountry = data.city.country;
     const srcFlagCountry = "https://purecatamphetamine.github.io/country-flag-icons/3x2/" + searchedCountry + ".svg";
-    const backgroundColorWhileSubmit = $(".window").css("background-color");
-
-    tempsDays = [];
-    iconDays = [];
-    iconCode = [];  // for dark/white mode
-
-    for (i = 8; i <= 32; i += 8) {
-        tempsDays.push(Math.floor(data.list[i - 1].main.temp));
-        iconCode.push(data.list[i - 1].weather[0].icon);
-
-        if (backgroundColorWhileSubmit === "rgb(18, 18, 18)") {
-            iconDays.push("images/darkmodeIcon/" + data.list[i - 1].weather[0].icon + ".svg");
-        } else {
-            iconDays.push("images/iconFutur/" + data.list[i - 1].weather[0].icon + ".svg");
-        }
-    }
 
     $(".loading").addClass("hidden");
     $("img").removeClass("hidden");
@@ -159,11 +166,20 @@ const successSubmitForecast = function (data) {
     $(".country").text(searchedCountry);
     $(".flag-img").attr("src", srcFlagCountry);
 
+    // Get the right icon / temp according to UTC-0 
+    const submitUTC = (data.city.timezone) / 3600;
+    const submitData = data;
+
+    tempsDays = []; // store the next 4 days temp
+    iconDays = []; // store the next 4 days icons
+    iconCode = [];  // for dark/white mode
+
+    handleForecastInfo(submitUTC, submitData);
+
     for (i = 1; i <= 4; i++) {
         $(".d" + i + "-temp").text(tempsDays[i - 1] + "°");
         $(".d" + i + "-img").attr("src", iconDays[i - 1]);
     }
-    /*  return iconCode; */
 }
 ////////////////////////////////////////////////////////////////////////////////////// API CALL SUBMIT
 
@@ -195,9 +211,84 @@ $("form").submit(e => {
     e.preventDefault();
 });
 
-// switch button Darkmode / Whitemode
+///////////////////////////////////// Deal with the right forecast info - "This is some serious GOURMET shit"
+function handleForecastInfo(searchedUTC, usedData) {
+    let d = new Date();
+    let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    const utc0Time = calcTime(0);
+
+    function calcTime(offset) {
+        let nd = new Date(utc + (3600000 * offset));
+        const optionsLocalTime = {
+            hour: "numeric"
+        };
+        const time = nd.toLocaleTimeString("fr-FR", optionsLocalTime);
+        const timeSliced = time.slice(0, 2);
+        return timeSliced;
+    }
+
+    if (utc0Time >= 21 && utc0Time < 0) { // first item 00H
+        handleSearchedUTC(17);
+    } else if (utc0Time >= 0 && utc0Time < 3) { //first item 3H
+        handleSearchedUTC(16);
+    } else if (utc0Time >= 3 && utc0Time < 6) { //first item 6H
+        handleSearchedUTC(15);
+    } else if (utc0Time >= 6 && utc0Time < 9) { //first item 9H
+        handleSearchedUTC(14);
+    } else if (utc0Time >= 9 && utc0Time < 12) { //first item 12H
+        handleSearchedUTC(13);
+    } else if (utc0Time >= 12 && utc0Time < 15) { //first item 15H
+        handleSearchedUTC(12);
+    } else if (utc0Time >= 15 && utc0Time < 18) { //first item 18H
+        handleSearchedUTC(11);
+    } else if (utc0Time >= 18 && utc0Time < 21) { //first item 21H
+        handleSearchedUTC(10);
+    }
+
+    function handleSearchedUTC(listNumer) {
+        if (searchedUTC === -14) {
+            getRightIcon(listNumer);
+        } else if (searchedUTC === -13 || searchedUTC === -12) {
+            getRightIcon(listNumer - 1);
+        } else if (searchedUTC === -11 || searchedUTC === -10 || searchedUTC === -9) {
+            getRightIcon(listNumer - 2);
+        } else if (searchedUTC === -8 || searchedUTC === -7 || searchedUTC === -6) {
+            getRightIcon(listNumer - 3);
+        } else if (searchedUTC === -5 || searchedUTC === -4 || searchedUTC === -3) {
+            getRightIcon(listNumer - 4);
+        } else if (searchedUTC === -2 || searchedUTC === -1 || searchedUTC === 0 || searchedUTC === 1) {
+            getRightIcon(listNumer - 5);
+        } else if (searchedUTC === 2 || searchedUTC === 3) {
+            getRightIcon(listNumer - 6);
+        } else if (searchedUTC === 4 || searchedUTC === 5 || searchedUTC === 6) {
+            getRightIcon(listNumer - 7);
+        } else if (searchedUTC === 7 || searchedUTC === 8 || searchedUTC === 9) {
+            getRightIcon(listNumer - 8);
+        } else if (searchedUTC === 10 || searchedUTC === 11 || searchedUTC === 12 || searchedUTC === 13 || searchedUTC === 14) {
+            getRightIcon(listNumer - 9);
+        }
+
+        function getRightIcon(first) {
+            for (i = first; i <= ((first + 8 * 3)); i += 8) {
+                tempsDays.push(Math.floor(usedData.list[i].main.temp));
+                iconCode.push(usedData.list[i].weather[0].icon);
+
+                const backgroundColorWhileSubmit = $(".window").css("background-color");
+
+                if (backgroundColorWhileSubmit === "rgb(18, 18, 18)") {
+                    iconDays.push("images/darkmodeIcon/" + usedData.list[i].weather[0].icon + ".svg");
+                } else {
+                    iconDays.push("images/iconFutur/" + usedData.list[i].weather[0].icon + ".svg");
+                }
+            }
+        }
+    }
+}
+
+//////////////////////////////////// switch button Darkmode / Whitemode
 $(".toggle-state").click(() => {
-    var backgroundColor = $(".window").css("background-color");
+    const backgroundColor = $(".window").css("background-color");
     if (backgroundColor === "rgb(255, 255, 255)") {
         darkMode();
     } else {
@@ -237,35 +328,42 @@ function whiteMode() {
     }
 }
 
-// Dynamic backbround
+/////////////////////////////////////// Dynamic backbround
 function getDynamicImage(todayIcon) {
-
+    const backgroundColor = $(".window").css("background-color");
     const windowSize = $(window).width();
 
     if (windowSize > 730) {
-        if (todayIcon === "01d" || todayIcon === "02d") {
-            $(".container-right").removeClass().addClass("bg-01d-02d container-right");
-        } else if (todayIcon === "01n" || todayIcon === "02n") {
-            $(".container-right").removeClass().addClass("bg-01n-02n container-right");
-        } else if (todayIcon === "03d" || todayIcon === "03n" || todayIcon === "04d" || todayIcon === "04n") {
-            $(".container-right").removeClass().addClass("bg-03d-03n-04d-04n container-right");
-        } else if (todayIcon === "09d" || todayIcon === "10d") {
-            $(".container-right").removeClass().addClass("bg-09d-10d container-right");
-        } else if (todayIcon === "09n" || todayIcon === "10n") {
-            $(".container-right").removeClass().addClass("bg-09n-10n container-right");
-        } else if (todayIcon === "11d" || todayIcon === "11n") {
-            $(".container-right").removeClass().addClass("bg-11d-11n container-right");
-        } else if (todayIcon === "13d" || todayIcon === "13n") {
-            $(".container-right").removeClass().addClass("bg-13d-13n container-right");
-        } else if (todayIcon === "50d" || todayIcon === "50n") {
-            $(".container-right").removeClass().addClass("bg-50d-50n container-right");
+        if (backgroundColor === "rgb(18, 18, 18)") {
+            handleOverlay("dark-overlay");
+        } else if (backgroundColor === "rgb(255, 255, 255)") {
+            handleOverlay();
         }
     } else {
-        $(".container-right").addClass("bg-image-smartphone");
+        $(".container-right").addClass("bg-image-smartphone"); // the image will not change in smartphone size
+    }
+
+    function handleOverlay(addOverlay) { // keep the dark overlay during darkmode while user submit several cities
+        if (todayIcon === "01d" || todayIcon === "02d") {
+            $(".container-right").removeClass().addClass("bg-01d-02d container-right").addClass(addOverlay);
+        } else if (todayIcon === "01n" || todayIcon === "02n") {
+            $(".container-right").removeClass().addClass("bg-01n-02n container-right").addClass(addOverlay);
+        } else if (todayIcon === "03d" || todayIcon === "03n" || todayIcon === "04d" || todayIcon === "04n") {
+            $(".container-right").removeClass().addClass("bg-03d-03n-04d-04n container-right ");
+        } else if (todayIcon === "09d" || todayIcon === "10d") {
+            $(".container-right").removeClass().addClass("bg-09d-10d container-right").addClass(addOverlay);
+        } else if (todayIcon === "09n" || todayIcon === "10n") {
+            $(".container-right").removeClass().addClass("bg-09n-10n container-right").addClass(addOverlay);
+        } else if (todayIcon === "11d" || todayIcon === "11n") {
+            $(".container-right").removeClass().addClass("bg-11d-11n container-right").addClass(addOverlay);
+        } else if (todayIcon === "13d" || todayIcon === "13n") {
+            $(".container-right").removeClass().addClass("bg-13d-13n container-right").addClass(addOverlay);
+        } else if (todayIcon === "50d" || todayIcon === "50n") {
+            $(".container-right").removeClass().addClass("bg-50d-50n container-right").addClass(addOverlay);
+        }
     }
 }
-
-////////////////////// Full-Screen mode
+//////////////////////////////////// Full-Screen mode
 function getFullscreenElement() {
     return document.fullscreenElement
         || document.webkitFullscreenElement
@@ -286,8 +384,3 @@ function toggleFullscreen() {
 document.addEventListener("dblclick", () => {
     toggleFullscreen();
 });
-
-/* $(document).ready(function () {
-    var windowHeight = $(window).innerHeight();
-    $('body').css({ 'height': windowHeight });
-}); */
